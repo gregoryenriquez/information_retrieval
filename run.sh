898d5f90cc95e05ec2b198f263206b1abfd6e36a
Gregory Enriquez <gregoryenriquez@gmail.com>
2016-04-11 04:12:54 -0700

    add runner, composer file

---
+++ run.sh
@@ -0,0 +1,72 @@
+
+
+echo "start clashroyale at alpha 0.0 query = best clash royale deck strategy\n"
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 0.0
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 0.1
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 0.2
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 0.3
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 0.4
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 0.5
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 0.6
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 0.7
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 0.8
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 0.9
+php search_program.php test/clashroyale/ "best clash royale deck strategy" bm25f stem 1.0
+
+echo "start election at alpha 0.0 query = Who will win the 2016 presidential election\n"
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 0.0
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 0.1
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 0.2
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 0.3
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 0.4
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 0.5
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 0.6
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 0.7
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 0.8
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 0.9
+php search_program.php test/election/ "Who will win the 2016 presidential election" bm25f stem 1.0
+
+echo "start investment at alpha 0.0 query = should invest money stocks\n"
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 0.0
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 0.1
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 0.2
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 0.3
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 0.4
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 0.5
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 0.6
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 0.7
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 0.8
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 0.9
+php search_program.php test/investment/ "should invest money stocks" bm25f stem 1.0
+
+echo "start masseffect at alpha 0.0 query = mass effect andromeda story\n"
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 0.0
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 0.1
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 0.2
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 0.3
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 0.4
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 0.5
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 0.6
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 0.7
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 0.8
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 0.9
+php search_program.php test/masseffect/ "mass effect andromeda story" bm25f stem 1.0
+
+echo "start tv at alpha 0.0 query = best 4k television\n"
+php search_program.php test/tv/ "best 4k television" bm25f stem 0.0
+php search_program.php test/tv/ "best 4k television" bm25f stem 0.1
+php search_program.php test/tv/ "best 4k television" bm25f stem 0.2
+php search_program.php test/tv/ "best 4k television" bm25f stem 0.3
+php search_program.php test/tv/ "best 4k television" bm25f stem 0.4
+php search_program.php test/tv/ "best 4k television" bm25f stem 0.5
+php search_program.php test/tv/ "best 4k television" bm25f stem 0.6
+php search_program.php test/tv/ "best 4k television" bm25f stem 0.7
+php search_program.php test/tv/ "best 4k television" bm25f stem 0.8
+php search_program.php test/tv/ "best 4k television" bm25f stem 0.9
+php search_program.php test/tv/ "best 4k television" bm25f stem 1.0
+
+
+
+
+
+
